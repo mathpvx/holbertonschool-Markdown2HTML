5d9717f6337c6c68fb7695e890dc0d88dd5ca947
mathpvx <7547@holbertonstudents.com>
2024-08-06 10:54:02 +0200

    convert md headings to html

--- markdown2html.py
+++ markdown2html.py
@@ -6,6 +6,21 @@ import os
 import sys
 
 
+def md2html(md_file, html_file):
+    """Converts heandings to HTML tags"""
+    with open(md_file, 'r') as md, open(html_file, 'w') as html:
+        for line in md:
+            # remove whitespaces
+            line = line.strip()
+            if line[0] == '#':
+                # count the nmber of leading '#' chars
+                level = len(line.split(' ')[0])
+                if 1 <= level <= 6:
+                    # extract the title
+                    title = line[level:].strip()
+                    html.write(f"<h{level}>{title}</h{level}>\n")
+
+
 def main():
     """Handles command line arguments and related errors"""
 
@@ -22,6 +37,8 @@ def main():
     if not os.path.isfile(md_file):
         print(f"Missing {md_file}", file=sys.stderr)
         sys.exit(1)
+
+    md2html(md_file, html_file)
 
     sys.exit(0)
 
