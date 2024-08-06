d1dd8df34263854def542ed90676444741d0b87a
mathpvx <7547@holbertonstudents.com>
2024-08-06 10:07:37 +0200

    pycodestyle

--- markdown2html.py
+++ markdown2html.py
@@ -5,21 +5,24 @@ Converts Markdown to HTML
 import os
 import sys
 
+
 def main():
-	"""Handles command line arguments and related errors"""
+    """Handles command line arguments and related errors"""
 
-	if len(sys.argv) < 3:
-		print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-		sys.exit(1)
-		
-	md_file = sys.argv[1]
-	html_file = sys.argv[2]
-		
-	if not os.path.isfile(md_file):
-		print(f"Missing {md_file}", file=sys.stderr)
-		sys.exit(1)
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md\
+            README.html", file=sys.stderr)
+        sys.exit(1)
 
-	sys.exit(0)
-		
+    md_file = sys.argv[1]
+    html_file = sys.argv[2]
+
+    if not os.path.isfile(md_file):
+        print(f"Missing {md_file}", file=sys.stderr)
+        sys.exit(1)
+
+    sys.exit(0)
+
+
 if __name__ == "__main__":
-	main()
+    main()
